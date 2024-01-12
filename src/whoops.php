511d0449a2cd28c609e000cdf14e77c739de0674
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-12 20:48:40 +0100

    seperate the Person class

--- src/whoops.php
+++ src/whoops.php
@@ -3,29 +3,12 @@
 
 require_once "../vendor/autoload.php";
 
+use App\models\Person;
+
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
-class Person {
-    private $name;
-    private $age;
-
-
-    public function __construct($name,$age)
-    {
-        $this->name = $name;
-        $this->age = $age;
-    }
-
-    public function getName(){
-        return $this->name;
-    }
-
-    public function getAge(){
-        return $this->age;
-    }
-}
 
 
 $persons = [
